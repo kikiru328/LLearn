047ed7dc78db28f4a11fa94d788aa94b3fd9aea7
kikiru328 <kikiru328@gmail.com>
2025-06-22 22:15:33 +0900

    feat(user): modified user entity
    - add field for default value
    - add active entity for is_active
    
    Issue: #3

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,21 +1,21 @@
-from dataclasses import dataclass
-from datetime import datetime
-from uuid import UUID
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
 from domain.value_objects.email import Email
 from domain.value_objects.password import Password
 
-@dataclass
+@dataclass(kw_only=True)
 class User:
     """User's domain entity"""
-    id: UUID
+    id: UUID = field(default_factory= uuid4)
     email: Email
     nickname: str
     hashed_password: Password
-    created_at: datetime
-    updated_at: datetime
     is_active: bool = True
     is_admin: bool = False
-    
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+
     def change_nickname(self, new_nickname: str) -> None:
         """Change User's nickname to new_nickname"""
         if not new_nickname or len(new_nickname.strip()) < 2:
@@ -23,7 +23,12 @@ class User:
         self.nickname = new_nickname.strip()
         self.updated_at = datetime.now()
 
+    def activate(self) -> None:
+        """activate"""
+        self.is_active = True
+        self.updated_at = datetime.now(timezone.utc)
+
     def deactivate(self) -> None:
         """soft delete, 계정 비활성화"""
         self.is_active = False
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now(timezone.utc)
