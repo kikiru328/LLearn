9f0860fd59d0cceeac99f9b2e603caf178aa9f83
kikiru328 <kikiru328@gmail.com>
2025-07-20 08:30:35 +0900

    test: based on TDD: Red for email vo

---
+++ tests/user/domain/value_object/test_email.py
@@ -0,0 +1,33 @@
+import pytest
+from user.domain.value_object.email import Email
+
+
+@pytest.mark.parametrize(
+    "raw",
+    [
+        "User@gamil.com",
+        "foo.bar-baz@sub.domain.com",
+    ],
+)
+def test_valid_email_normalize_to_lowercase(raw) -> None:
+    email = Email(raw)
+    assert str(email) == raw.lower()
+
+
+@pytest.mark.parametrize(
+    "raw",
+    [
+        "invalid@",
+        "foo@.com",
+        "bar.com",
+        "",
+    ],
+)
+def test_invalid_email_raises_value_error(raw) -> None:
+    with pytest.raises(ValueError):
+        Email(raw)
+
+
+def test_email_equlity():
+    assert Email("A@b.com") == Email("a@B.com")
+    assert {Email("x@y.com")} == {Email("X@Y.COM")}
