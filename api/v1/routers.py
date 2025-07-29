82ad7e12ad30d5bbb126406f8a812b213f3f70e8
kikiru328 <kikiru328@gmail.com>
2025-07-29 22:50:04 +0900

    feat(admin): admin delete user by name

--- api/v1/routers.py
+++ api/v1/routers.py
@@ -9,6 +9,6 @@ from admin.interface.controllers.admin_controller import router as admin_router
 v1_router = APIRouter(prefix="/api/v1")
 
 v1_router.include_router(auth_router)
+v1_router.include_router(admin_router)
 v1_router.include_router(user_router)
 v1_router.include_router(curriculum_router)
-v1_router.include_router(admin_router)
