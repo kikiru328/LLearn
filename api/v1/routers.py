577e9fea4c529af8c4034243b4a8628f087e826b
kikiru328 <kikiru328@gmail.com>
2025-08-01 18:57:13 +0900

    feat(admin): admin api for summary

--- api/v1/routers.py
+++ api/v1/routers.py
@@ -17,12 +17,16 @@ from admin.interface.controllers.admin_user_controller import (
 from admin.interface.controllers.admin_curriculum_controller import (
     router as admin_curriculum_controller,
 )
+from admin.interface.controllers.admin_summary_controller import (
+    router as admin_summary_controller,
+)
 
 v1_router = APIRouter(prefix="/api/v1")
 
 v1_router.include_router(auth_router)
 v1_router.include_router(admin_user_router)
 v1_router.include_router(admin_curriculum_controller)
+v1_router.include_router(admin_summary_controller)
 v1_router.include_router(user_router)
 v1_router.include_router(curriculum_router)
 v1_router.include_router(user_summary_router)
